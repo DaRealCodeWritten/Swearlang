319d306dd69e9dc899c24d8b81bc09c69539a6ee
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-10-23 06:05:27 +0000

    Add interpreted mode

--- dexport_to_unix.sh
+++ dexport_to_unix.sh
@@ -1,5 +1,5 @@
-echo "Removed Swearlang"
+echo "Removing Swearlang"
 rm /usr/bin/swear
-rm -rf /usr/bin/errors
+rm -r /usr/bin/errors
 rm /usr/bin/__init__.py
 echo "Removed Swearlang"
--- swear.py
+++ swear.py
@@ -10,27 +10,39 @@ keywords = {
     "Cunt": "else",
 }
 symbols = {
-    "fuck ": "'",
-    "Fuck ": "\"",
-    "bitch ": "(",
-    "Bitch ": ")",
-    "motherfucker": "=",
-    "Motherfucker": "!",
+    "shitter": "f",
+    "fuck": "'",
+    "Fuck": "\"",
+    "bitch": "(",
+    "Bitch": ")",
+    "motherfcker": "=",
+    "Motherfcker": "!",
     "bullshit": "<",
     "Bullshit": ">",
 }
+
+
+def replacer(string: str):
+    for find, replace in keywords.items():
+        string = string.replace(find, replace)
+    for find, replace in symbols.items():
+        string = string.replace(find, replace)
+    return string
 
 
 if "--file" in sys.argv:
     fileind = sys.argv.index("--file")
     try:
         filename = sys.argv[fileind+1]
+        if "--interpreter-mode" in sys.argv:
+            with open(filename) as stream:
+                for line in stream:
+                    parse = replacer(line)
+                    exec(parse)
+            exit(0)
         with open(filename) as file:
             code = file.read()
-        for find, replace in keywords.items():
-            code = code.replace(find, replace)
-        for find, replace in symbols.items():
-            code = code.replace(find, replace)
-        eval(code)
+        parsed = replacer(code)
+        exec(parsed)
     except IndexError as error:
         raise errors.MissingArgumentsError("Missing file name") from error
