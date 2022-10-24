f90a885de74fcf9b19c2c055de31e6a3addbb90b
Arszilla <arszilla@arszilla.com>
2022-10-25 00:07:41 +0300

    Improvements and fixes (#1)
    * Add shebang and improve formatting
    
    * Improve syntax and formatting
    
    * Add shebang and improve formatting
    
    * Add shebang and improve formatting

--- install_for_linux.sh
+++ install_for_linux.sh
@@ -1,24 +1,38 @@
+#!/bin/bash
+
 /usr/bin/echo "Installing Swearlang to /usr/bin"
+
 case $1 in
     source)
         /usr/bin/echo "Installing source"
         /usr/bin/mkdir /usr/bin/Swearlang
         /usr/bin/cp ./swear.py /usr/bin/Swearlang/swear-dev
         /usr/bin/cp ./errors/errors.py /usr/bin/Swearlang
+
         if [ $(/usr/bin/whoami) == root ] ; then
             /usr/bin/chmod 755 /usr/bin/Swearlang/swear-dev
+
         else
             /usr/bin/echo "Could not make swear-dev executable, run \"/usr/bin/chmod 755 /usr/bin/Swearlang/swear-dev\""
+
         fi
-    ;;
+
+        ;;
+
     *)
         /usr/bin/echo "Installing compiled binary"
         /usr/bin/cp ./swear /usr/bin/swear
+
         if [ $(/usr/bin/whomai) == root ] ; then
             /usr/bin/chmod +x /usr/bin/swear
+
         else
             /usr/bin/echo "Could not make swear executable, run \"/usr/bin/chmod +x /usr/bin/swear\""
+
         fi
-     ;;
+
+        ;;
+
 esac
+
 /usr/bin/echo "Installed Swearlang"
