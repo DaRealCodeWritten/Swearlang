31baece4efa6f7d01c554da0724b86371836d867
Emma Lysne <SSwiftGamer440@gmail.com>
2022-10-24 11:54:38 -0700

    Fix equality checks and add exit message

--- export_to_unix.sh
+++ export_to_unix.sh
@@ -5,7 +5,7 @@ case $1 in
         /usr/bin/mkdir /usr/bin/Swearlang
         /usr/bin/cp ./swear.py /usr/bin/Swearlang/swear-dev
         /usr/bin/cp ./errors/errors.py /usr/bin/Swearlang
-        if [ /usr/bin/whoami != root ] ; then
+        if [ $(/usr/bin/whoami) == root ] ; then
             /usr/bin/chmod 755 /usr/bin/Swearlang/swear-dev
         else
             /usr/bin/echo "Could not make swear-dev executable, run \"/usr/bin/chmod 755 /usr/bin/Swearlang/swear-dev\""
@@ -14,10 +14,11 @@ case $1 in
     *)
         /usr/bin/echo "Installing compiled binary"
         /usr/bin/cp ./swear /usr/bin/swear
-        if [ /usr/bin/whomai != root ] ; then
+        if [ $(/usr/bin/whomai) == root ] ; then
             /usr/bin/chmod +x /usr/bin/swear
         else
             /usr/bin/echo "Could not make swear executable, run \"/usr/bin/chmod +x /usr/bin/swear\""
         fi
      ;;
 esac
+echo "Installed Swearlang"
