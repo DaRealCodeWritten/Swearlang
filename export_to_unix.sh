530649269e0924e51fe62be3604ea1ae624e5e3f
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-10-24 08:55:44 +0000

    Add args to installer and secure paths

--- export_to_unix.sh
+++ export_to_unix.sh
@@ -1,8 +1,23 @@
-echo "Installing Swearlang to /usr/bin"
-cp ./swear /usr/bin
-if [ whoami != root ]
-    then chmod +x /usr/bin/swear
-else
-    echo "Unable to make /usr/bin/swear executable, please run \"sudo chmod +x /usr/bin/swear\""
-fi
-echo "Installed Swearlang"
+/usr/bin/echo "Installing Swearlang to /usr/bin"
+case $1 in
+    source)
+        /usr/bin/echo "Installing source"
+        /usr/bin/mkdir /usr/bin/Swearlang
+        /usr/bin/cp ./swear.py /usr/bin/Swearlang/swear-dev
+        /usr/bin/cp ./errors/errors.py /usr/bin/Swearlang
+        if [ /usr/bin/whoami != root ] ; then
+            /usr/bin/chmod 755 /usr/bin/Swearlang/swear-dev
+        else
+            /usr/bin/echo "Could not make swear-dev executable, run \"/usr/bin/chmod 755 /usr/bin/Swearlang/swear-dev\""
+        fi
+    ;;
+    *)
+        /usr/bin/echo "Installing compiled binary"
+        /usr/bin/cp ./swear /usr/bin/swear
+        if [ /usr/bin/whomai != root ] ; then
+            /usr/bin/chmod +x /usr/bin/swear
+        else
+            /usr/bin/echo "Could not make swear executable, run \"/usr/bin/chmod +x /usr/bin/swear\""
+        fi
+     ;;
+esac
