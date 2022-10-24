26a2b77ca93480c18d9c62932b4cfafa56f487d2
Arszilla <arszilla@arszilla.com>
2022-10-25 00:02:33 +0300

    Add shebang and improve formatting

--- uninstall_for_linux.sh
+++ uninstall_for_linux.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
+
 /usr/bin/echo "Uninstalling Swearlang from /usr/bin"
+
 case $1 in
     source)
         /usr/bin/echo "Removing source"
         /usr/bin/rm -r /usr/bin/Swearlang
-    ;;
+
+        ;;
+
     *)
         /usr/bin/echo "Removing compiled binary"
         /usr/bin/rm /usr/bin/swear
-     ;;
+
+        ;;
+
 esac
+
 /usr/bin/echo "Uninstalled Swearlang"
