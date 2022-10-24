40b4ea5cea52de272d5363732ab13dd363ab208a
Arszilla <arszilla@arszilla.com>
2022-10-25 00:04:09 +0300

    Add shebang and improve formatting

--- create_release.sh
+++ create_release.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 cp export_to_unix.sh ./release
 cp dexport_to_unix.sh ./release
 cp ./dist/swear* ./release
 zip Swearlang.zip ./release
 mv Swearlang.zip /sdcard
+
 echo "Finished creating zipfile"
