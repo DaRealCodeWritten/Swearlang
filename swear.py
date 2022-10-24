e024a7015a447abf5931b9f7e0443aee15adc4ad
Arszilla <arszilla@arszilla.com>
2022-10-25 00:02:37 +0300

    Improve syntax and formatting

--- swear.py
+++ swear.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import sys
+
 import errors
 
-
 keywords = {
-    "shit ": "print",
-    "Shit ": "input",
+    "shit": "print",
+    "Shit": "input",
     "cunt": "if",
     "Cunt": "else",
 }
+
 symbols = {
     "shitter": "f",
     "fuck": "'",
@@ -25,24 +26,33 @@ symbols = {
 def replacer(string: str):
     for find, replace in keywords.items():
         string = string.replace(find, replace)
+
     for find, replace in symbols.items():
         string = string.replace(find, replace)
+
     return string
 
 
-if "--file" in sys.argv:
-    fileind = sys.argv.index("--file")
-    try:
-        filename = sys.argv[fileind+1]
-        if "--interpreter-mode" in sys.argv:
-            with open(filename) as stream:
-                for line in stream:
-                    parse = replacer(line)
-                    exec(parse)
-            exit(0)
-        with open(filename) as file:
-            code = file.read()
-        parsed = replacer(code)
-        exec(parsed)
-    except IndexError as error:
-        raise errors.MissingArgumentsError("Missing file name") from error
+if __name__ == "__main__":
+    if "--file" in sys.argv:
+        fileind = sys.argv.index("--file")
+
+        try:
+            filename = sys.argv[fileind+1]
+
+            if "--interpreter-mode" in sys.argv:
+                with open(filename) as stream:
+                    for line in stream:
+                        parse = replacer(line)
+                        exec(parse)
+
+                exit(0)
+
+            with open(filename) as file:
+                code = file.read()
+
+            parsed = replacer(code)
+            exec(parsed)
+
+        except IndexError as error:
+            raise errors.MissingArgumentsError("Missing file name") from error
