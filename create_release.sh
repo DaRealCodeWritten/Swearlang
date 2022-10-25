2da06b271ce32d667cf83dd0a6b5685cb6554875
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-10-25 21:19:22 +0000

    Fix release script to actually put files in the zip

--- create_release.sh
+++ create_release.sh
@@ -6,7 +6,7 @@ pyinstaller --onefile swear.py __init__.py ./errors/errors.py
 cp install_for_linux.sh ./release
 cp uninstall_for_linux.sh ./release
 cp ./dist/swear* ./release
-zip Swearlang.zip ./release
+zip Swearlang.zip ./release/*
 mv Swearlang.zip /sdcard
 
 echo "Finished creating zipfile"
